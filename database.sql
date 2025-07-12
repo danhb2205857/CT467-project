8cd46f334cb23b026c4c27cdb3b7a5ac93d52bac
danhtam1168 <danhtam1168@gmail.com>
2025-07-13 00:05:27 +0700

    sql

--- database.sql
+++ database.sql
@@ -5,6 +5,7 @@ CREATE TABLE admins (
   id int(11) NOT NULL AUTO_INCREMENT,
   email varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   password varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  name varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id),
