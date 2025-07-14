474dda57a9763cb93380134b7310787a020a0e17
danhtam1168 <danhtam1168@gmail.com>
2025-07-14 16:24:41 +0700

    enum

---
+++ app/Constants/BorrowSlipDetailStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Constants;
+
+class BorrowSlipDetailStatus
+{
+    const DANG_MUON = 0;   // Đang mượn
+    const DA_TRA = 1;      // Đã trả
+    const QUA_HAN = 2;     // Quá hạn
+} 
---
+++ app/Constants/BorrowSlipStatus.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Constants;
+
+class BorrowSlipStatus
+{
+    const CHUA_HOAN_THANH = 0; // Chưa hoàn thành
+    const HOAN_THANH = 1;      // Hoàn thành
+} 
